4a2527790078fc92708276061f7182ff10663a58
Casey Price <casey@leaflogix.com>
2020-12-16 10:30:19 -0500

    added day 8, 9, 10 and 11

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
-﻿using AdventOfCode2020.Day_2;
+﻿using AdventOfCode2020.Day_10;
+using AdventOfCode2020.Day_11;
+using AdventOfCode2020.Day_2;
 using AdventOfCode2020.Day_3;
 using AdventOfCode2020.Day_4;
 using AdventOfCode2020.Day_5;
 using AdventOfCode2020.Day_6;
 using AdventOfCode2020.Day_7;
+using AdventOfCode2020.Day_8;
+using AdventOfCode2020.Day_9;
 using System;
 
 namespace AdventOfCode2020 {
@@ -30,6 +34,17 @@ namespace AdventOfCode2020 {
 
             Console.WriteLine($"Day 7: {new Day7().Solve()}");
             Console.WriteLine($"Day 7 Bonus: {new Day7().SolveBonus()}");
+
+            Console.WriteLine($"Day 8: {new Day8().Solve()}");
+            Console.WriteLine($"Day 8 Bonus: {new Day8().SolveBonus()}");
+
+            Console.WriteLine($"Day 9: {new Day9().Solve()}");
+            Console.WriteLine($"Day 9 Bonus: {new Day9().SolveBonus()}");
+
+            Console.WriteLine($"Day 10: {new Day10().Solve()}");
+            Console.WriteLine($"Day 10 Bonus: {new Day10().SolveBonus()}");
+
+            Console.WriteLine($"Day 11: {new Day11().Solve()}");
         }
 
     }
